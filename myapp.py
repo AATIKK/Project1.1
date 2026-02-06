95e77b4493e67aecf17f897929da1383951d47a8
Atik Masud Kanak <atikmasud2002@gmail.com>
2026-02-06 10:11:03 +0600

    Refactor imports and update Streamlit title

--- myapp.py
+++ myapp.py
@@ -1,10 +1,13 @@
-#framework
+
+# Framework imports
 import streamlit as st
-import pandas 
-import numpy
+import pandas as pd
+import numpy as np
 import matplotlib.pyplot as plt
+
 from sklearn.preprocessing import StandardScaler
 from sklearn.model_selection import train_test_split
 
-st.title("Good to Go,buddy")
+# Streamlit UI
+st.title("Good to Go, buddy 🚀")
 st.subheader("ML with me")
