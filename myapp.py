45f76e65e26537f9507733aff389e8af9aacc5e2
Atik Masud Kanak <atikmasud2002@gmail.com>
2026-02-06 11:48:00 +0600

    Update myapp.py

--- myapp.py
+++ myapp.py
@@ -4,7 +4,7 @@ import streamlit as st
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
-
+import seaborn as sns
 from sklearn.preprocessing import StandardScaler
 from sklearn.model_selection import train_test_split
 
@@ -16,3 +16,18 @@ st.subheader("ML with me")
 # create sidebar
 st.sidebar.header("upload CSV data or use sample")
 use_example = st.sidebar.checkbox("Use example dataset")
+#load dataset.......
+if user_example:
+  df = sns.load_dataset('tips')
+
+  df = df.dropna()
+  st.success("loaded sample dataset: 'tips'")
+  
+
+else:
+  uploaded_file = st.sidebar.file_uploader("Upload your csv file",type['csv'])
+  if uploaded_file:
+    df = pd.read_csv(uploaded_file)
+  else:
+    st.waning("please upload a csv file or use example")
+    st.stop()
