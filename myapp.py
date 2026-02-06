137e158fab9fe1cf4f245fc98d0cb2d22e62ad57
Atik Masud Kanak <atikmasud2002@gmail.com>
2026-02-06 10:08:06 +0600

    Add imports and update Streamlit title and subheader

--- myapp.py
+++ myapp.py
@@ -1,3 +1,10 @@
 #framework
 import streamlit as st
+import pandas 
+import numpy
+import matplotlib as plt
+from sklearn.preprocessing import StadardScaler
+from sklearn.model_selection import train_test_split
+
 st.title("Good to Go,buddy")
+st.subheader("ML with me")
